5abb7152c4b7eb5123db29cae2851c3eedae7c5b
Okwor Okechukwu Joshua <joshuaokwor565@icloud.com>
2023-03-19 17:44:42 -0700

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-"""
-This script lists all states from the
+s script lists all states from the
 database `hbtn_0e_0_usa`.
 """
 
@@ -12,7 +10,7 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
-:   db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
